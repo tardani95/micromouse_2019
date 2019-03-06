0c58627da0238dec64f1c1524d31d102105f1161
Daniel Tar <tardani95@gmail.com>
2019-03-06 12:28:41 +0100

    project file set to compile and link library files

---
+++ 03_software/embedded_sw_SPL_C/ilb/test_lib.c
@@ -0,0 +1,10 @@
+/*
+ * test_lib.c
+ *
+ *  Created on: Mar 6, 2019
+ *      Author: tard
+ */
+
+#include "test_lib.h"
+
+
---
+++ 03_software/embedded_sw_SPL_C/ilb/test_lib.h
@@ -0,0 +1,13 @@
+/*
+ * test_lib.h
+ *
+ *  Created on: Mar 6, 2019
+ *      Author: tard
+ */
+
+#ifndef TEST_LIB_H_
+#define TEST_LIB_H_
+
+/* library header code here */
+
+#endif /* TEST_LIB_H_ */
