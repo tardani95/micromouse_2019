040c4b2d2b575bceffd59bd0a0f5fe7b9dc346fb
RZ <resizoltan@live.com>
2019-03-10 00:48:28 +0100

    Menu macros added, main menu defined

--- 03_software/embedded_sw_SPL_C/lib/inc/sw_menu.h
+++ 03_software/embedded_sw_SPL_C/lib/inc/sw_menu.h
@@ -15,8 +15,66 @@
 /** @addtogroup menu
  * @{
  */
+typedef struct item item_t;
+typedef item_t* item_p_t;
+typedef item_p_t* item_it_t;
+typedef item_p_t* item_parr_t;
 
-//TODO user code here
+typedef struct menu menu_t;
+typedef menu_t* menu_p_t;
+
+typedef void(*onSelectCallback_t)(menu_p_t);
+
+struct item {
+	char* name_;
+	onSelectCallback_t onSelectCallback_;
+	item_parr_t *items_;
+	item_it_t first_;
+	item_it_t item_iterator_;
+	item_it_t last_item_;
+};
+
+struct menu {
+	item_p_t curr_item_;// = NULL;
+};
+
+#define NUMARGS(...)  (sizeof((int[]){ __VA_ARGS__})/sizeof(int))
+
+#define SUBMENU(name, ...)									\
+	item_p_t name##_subitems[] = {NULL , __VA_ARGS__};		\
+	item_t name = {											\
+		.name_ = #name,										\
+		.onSelectCallback_ = enterSubmenu,					\
+		.items_ = &name##_subitems,							\
+		.first_ = &name##_subitems[0],						\
+		.item_iterator_= &name##_subitems[0],				\
+		.last_item_= &name##_subitems[NUMARGS(__VA_ARGS__)]	\
+	}
+
+#define ACTION(name, onSelectCallback)						\
+	item_t name = {											\
+		.name_ = #name,										\
+		.onSelectCallback_ = onSelectCallback,				\
+		.items_ = NULL,										\
+		.first_ = NULL,										\
+		.item_iterator_= NULL,								\
+		.last_item_= NULL									\
+	}
+
+#define MENU(name, root)									\
+	menu_t name = {											\
+		.curr_item_ = root									\
+	}
+
+
+void initMenu(menu_p_t menu_p);
+void initNextItem(item_p_t parent_p, item_p_t current_p);
+
+void incrementItemIterator(item_p_t item_p);
+void resetItemIterator(item_p_t item_p);
+
+void selectItem(menu_p_t menu_p);
+void nextItem(menu_p_t menu_p);
 /**
  * @}
  * end of menu
--- 03_software/embedded_sw_SPL_C/lib/src/sw_menu.c
+++ 03_software/embedded_sw_SPL_C/lib/src/sw_menu.c
@@ -6,7 +6,7 @@
  ************************/
 
 #include "sw_menu.h"
-
+#include "stddef.h"
 /** @addtogroup software_modules
  * @{
  */
@@ -15,6 +15,86 @@
  * @brief menu system
  * @{
  */
+
+void initMenu(menu_p_t menu_p) {
+	initNextItem(menu_p->curr_item_, menu_p->curr_item_);
+	return;
+}
+
+void initNextItem(item_p_t parent_p, item_p_t current_p) {
+
+	if (current_p->first_ == 0)
+		return;
+
+	*current_p->first_ = parent_p;
+	incrementItemIterator(current_p);
+	while (current_p->item_iterator_ != current_p->first_) {
+		initNextItem(current_p, *current_p->item_iterator_);
+		incrementItemIterator(current_p);
+	}
+	return;
+}
+
+void incrementItemIterator(item_p_t item_p) {
+	item_it_t *it_p = &(item_p->item_iterator_);
+	if (*it_p != item_p->last_item_) {
+		(*it_p)++;
+	}
+	else{
+		*it_p = item_p->first_;
+	}
+	return;
+}
+
+void resetItemIterator(item_p_t item_p) {
+	item_p->item_iterator_ = item_p->first_;
+	incrementItemIterator(item_p);
+	return;
+}
+
+void selectItem(menu_p_t menu_p) {
+	item_it_t it = menu_p->curr_item_->item_iterator_;
+	(*it)->onSelectCallback_(menu_p);
+	return;
+}
+
+void nextItem(menu_p_t menu_p) {
+	incrementItemIterator(menu_p->curr_item_);
+	//printf("Looking at: ");
+	//printf((*menu_p->curr_item_->item_iterator_)->name_);
+	//printf("\n");
+	return;
+}
+
+void enterSubmenu(menu_p_t menu_p) {
+	item_p_t old_item_p = menu_p->curr_item_;
+	item_p_t new_item_p = *(old_item_p->item_iterator_);
+	resetItemIterator(new_item_p);
+	menu_p->curr_item_ = new_item_p;
+	//printf("Entered ");
+	//printf(menu_p->curr_item_->name_);
+	//printf("\nLooking at: ");
+	//printf((*menu_p->curr_item_->item_iterator_)->name_);
+	//printf("\n");
+	return;
+}
+
+
+SUBMENU(explore);
+SUBMENU(speedrun);
+SUBMENU(run, &explore, &speedrun);
+SUBMENU(algorithm);
+SUBMENU(manoeuvre);
+SUBMENU(control_parameters);
+SUBMENU(speedrun_config, &algorithm, &manoeuvre, &control_parameters);
+SUBMENU(direct_route_algorithm);
+SUBMENU(map_extension_method);
+SUBMENU(map_control);
+SUBMENU(explore_config, &direct_route_algorithm, &map_extension_method, &map_control);
+SUBMENU(configure, &explore_config, &speedrun_config);
+SUBMENU(root, &configure, &run);
+
+MENU(main_menu, &root);
 
 //TODO user code here
 /**
