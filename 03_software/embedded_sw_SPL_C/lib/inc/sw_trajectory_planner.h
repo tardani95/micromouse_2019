1c167b768e311912c7c9f944269966b95da60d3a
Daniel Tar <tardani95@gmail.com>
2019-08-04 15:31:22 +0200

    trajecorty planner module added

---
+++ 03_software/embedded_sw_SPL_C/lib/inc/sw_trajectory_planner.h
@@ -0,0 +1,28 @@
+/***********************
+ * @file 	sw_trajectory_planner.h
+ * @author	tard
+ * @date	Aug 04, 2019
+ * @version	vx_x
+************************/
+
+#ifndef SW_TRAJECTORY_PLANNER_H_
+#define SW_TRAJECTORY_PLANNER_H_
+
+/** @addtogroup software_modules
+ * @{
+ */
+
+/** @addtogroup Trajectory_Planner
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
+
+#endif /* SW_TRAJECTORY_PLANNER_H_ */
+
---
+++ 03_software/embedded_sw_SPL_C/lib/src/sw_trajectory_planner.c
@@ -0,0 +1,24 @@
+/***********************
+ * @file 	sw_trajectory_planner.c
+ * @author	tard
+ * @date	Aug 4, 2019
+ * @version	vx_x
+************************/
+
+#include "sw_trajectory_planner.h"
+
+/** @addtogroup software_modules
+ * @{
+ */
+
+/** @defgroup Trajectory_Planner Trajectory Planner
+ * @{
+ */
+
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
